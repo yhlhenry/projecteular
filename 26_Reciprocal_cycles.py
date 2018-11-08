9f577abdf535e8e91244f1a19d11ca44ce0cef6f
yhlhenry <33752961+yhlhenry@users.noreply.github.com>
2018-11-08 17:08:11 +0800

    Update 26_Reciprocal_cycles.py

--- 26_Reciprocal_cycles.py
+++ 26_Reciprocal_cycles.py
@@ -1,9 +1,21 @@
+# 1. first glance solution, not so robust
 from decimal import *
 getcontext().prec = 3000
-itv = 10
-
-for i in range(1000):
+digit_list = [0]
+for i in range(1001):
     r = str(Decimal(1) / Decimal(i + 1))
+    digit_list.append(r.find(r[5:11], 11))
+print(digit_list.index(max(digit_list)))
 
-    if r.find(r[5:11],11) > 986:
-        print(i+1, r, r.find(r[5:11],11))
+
+# 2. another better solution
+digit_list = [0]
+remainder_list = []
+for i in range(1, 1001):
+    last_r = 1 % i
+    while last_r not in remainder_list:
+        remainder_list.append(last_r)
+        last_r = (last_r * 10) % i
+    digit_list.append(len(remainder_list))
+    remainder_list.clear()
+print(digit_list.index(max(digit_list)))
